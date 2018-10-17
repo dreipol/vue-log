0a128c3bc56f37c113ca284ba29beb88349905af
Rouven Bühlmann <rouven.buehlmann@dreipol.ch>
2018-10-17 09:58:10 +0200

    Update eslint-config and fix lint command

--- test/runner.js
+++ test/runner.js
@@ -1,7 +1,7 @@
 require('jsdom-global')();
 
 const chai = require('chai');
-const chaiSpies = require('chai-spies');
+// const chaiSpies = require('chai-spies');
 chai.use(require('chai-spies'));
 
 const Vue = require('vue');
